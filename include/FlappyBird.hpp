ac88d764b59e99e9353adaa74bb8307993b9a09a
BillyFrcs <69786531+BillyFrcs@users.noreply.github.com>
2021-07-24 17:53:41 +0900

    Update FlappyBird definition

--- include/FlappyBird.hpp
+++ include/FlappyBird.hpp
@@ -39,10 +39,10 @@ enum WindowScreenSize
 
 /*Flappy bird definition to load assets from file path*/
 // Background and main menu /////////////////////////////////////////////////////
-#define SPLASH_BACKGROUND_PATH "assets/textures/game-states/FlappySky.png"
-#define MAIN_MENU_BACKGROUND_GAME_PATH "assets/textures/game-states/FlappySky.png"
-#define GAME_BACKGROUND_PATH "assets/textures/game-states/FlappySky.png"
-#define GAME_OVER_BACKGROUND_PATH "assets/textures/game-states/FlappySky.png"
+#define SPLASH_BACKGROUND_PATH "assets/textures/game-states/FlappyDay.png"
+#define MAIN_MENU_BACKGROUND_GAME_PATH "assets/textures/game-states/FlappyDay.png"
+#define GAME_BACKGROUND_PATH "assets/textures/game-states/FlappyNight.png"
+#define GAME_OVER_BACKGROUND_PATH "assets/textures/game-states/FlappyNight.png"
 //////////////////////////////////////////////////////////////////////////////////
 
 // Title and button //////////////////////////////////////////////////////
@@ -51,8 +51,8 @@ enum WindowScreenSize
 //////////////////////////////////////////////////////////////////////////
 
 // Pipes /////////////////////////////////////////////////////
-#define PIPE_UP_FILEPATH "assets/textures/pipes/PipeUp.png"
-#define PIPE_DOWN_FILEPATH "assets/textures/pipes/PipeDown.png"
+#define PIPE_UP_FILEPATH "assets/textures/pipes/PipeUp2.png"
+#define PIPE_DOWN_FILEPATH "assets/textures/pipes/PipeDown2.png"
 //////////////////////////////////////////////////////////////
 
 // Land /////////////////////////////////////////////////////
